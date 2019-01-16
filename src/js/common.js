f589b4e3b0350c2aaf612fa7a9b1d95ced1ebee0
Your Name <semchukat@gmail.com>
2019-01-16 12:54:59 +0200

    #2 issue, refactor 'getKittenCardCollor' function

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,6 @@
 'use strict';
 
-getKittens('https://ma-cats-api.herokuapp.com/api/cats?page=1&per_page=12')
+getKittens('https://ma-cats-api.herokuapp.com/api/cats?page=5&per_page=16')
     .then(function(catsObject) {
         document.querySelector('.wrapper').insertAdjacentHTML('afterbegin', renderKittenList(catsObject.cats));
         setTimeout(() => document.querySelector('.loader').classList.toggle('loader_active'), 500);
@@ -17,25 +17,24 @@ function renderKittenList(catsList) {
     return catsList.map((cat, i) => renderKitten(cat, i)).join('');
 }
 
-function renderKitten({id, name, img_url, category, price}, indexCat) {
-    let index = 0;
+function renderKitten(kitten, indexCat) {
     return `
         <div class="kitty-card kitty-card_margin_small" style="animation-delay: ${.5 + indexCat * .05}s">
-            <div class="kitty-card__header" style="background-color: ${renderColorByHash(name, id)};">
-                <img class="kitty-card__image" src="${img_url}" alt="${name}">
+            <div class="kitty-card__header" style="background-color: ${getKittenCardCollor(kitten)}">
+                <img class="kitty-card__image" src="${kitten.img_url}" alt="${kitten.name}">
                 <div class="kitty-card__name">
-                    <span class="name name_size_medium name_bg_white">${name}</span>
+                    <span class="name name_size_medium name_bg_white">${kitten.name}</span>
                 </div>
             </div>
-            <div class="kitty-card__id"># ${id}</div>
-            <div class="kitty-card__category">${category}</div>
-            <div class="kitty-card__price">$ ${price}</div>
+            <div class="kitty-card__id"># ${kitten.id}</div>
+            <div class="kitty-card__category">${kitten.category}</div>
+            <div class="kitty-card__price">$ ${kitten.price}</div>
         </div>
         `;
 }
 
-function renderColorByHash(...tokens) {
-    const tokenString = [tokens].reduce((string, token) => string + token, '')
+function getKittenCardCollor(kitten) {
+    const tokenString = kitten.name + kitten.id;
 
     // md5 function from github.com/blueimp/JavaScript-MD5
     const hash = md5(tokenString);
